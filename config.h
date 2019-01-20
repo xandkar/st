278adc1435eee65e655b629cbe694e3980e67217
Siraaj Khandkar <siraaj@khandkar.net>
2019-01-19 19:10:45 -0500

    Increase font size for ThinkPad T570 HiDPI screen

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Terminus:pixelsize=14:antialias=false:autohint=false";
+static char *font = "Terminus:pixelsize=32:antialias=false:autohint=false";
 static int borderpx = 0;
 
 /*
